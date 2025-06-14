f1dd5820294513fb9164c78e4ad59fa318104d65
Alun Jones <alunacjones@gmail.com>
2025-06-14 22:39:18 +0100

    tidy up

--- test/LSL.Sentinet.ApiClient.Tests/FoldersFacadeTests.cs
+++ test/LSL.Sentinet.ApiClient.Tests/FoldersFacadeTests.cs
@@ -10,55 +10,39 @@ public class FoldersFacadeTests : BaseTest
     [Test]
     public async Task GivenAValidRequestForAFolder_ItShouldReturnTheExpectedResult()
     {
-        try
-        {
-            // Arrange
-            var provider = CreateServiceProvider(fakeService: true);
-            var sut = provider.GetRequiredService<IFoldersFacade>();
-            var folder = "Test/Folder";
-            var folderResults = provider.StubFolderCalls(folder);
+        // Arrange
+        var provider = CreateServiceProvider(fakeService: true);
+        var sut = provider.GetRequiredService<IFoldersFacade>();
+        var folder = "Test/Folder";
+        var folderResults = provider.StubFolderCalls(folder);
 
-            // Act
-            var response = await sut.GetFolderAsync(folder);
+        // Act
+        var response = await sut.GetFolderAsync(folder);
 
-            // Assert
-            using var assertionScope = new AssertionScope();
+        // Assert
+        using var assertionScope = new AssertionScope();
 
-            response.SubTree.Should().Be(folderResults.ElementAt(2));
-            response.FullPath.Should().Be("Test/Folder");
-            response.Parent.SubTree.Should().Be(folderResults.ElementAt(1));
-            response.Parent.FullPath.Should().Be("Test");
-            response.Parent.Parent.SubTree.Should().Be(folderResults.ElementAt(0));
-            response.Parent.Parent.FullPath.Should().Be("");
-
-        }
-        catch (Exception ex)
-        {
-
-            throw new ArgumentException("ad", ex);
-        }
+        response.SubTree.Should().Be(folderResults.ElementAt(2));
+        response.FullPath.Should().Be("Test/Folder");
+        response.Parent.SubTree.Should().Be(folderResults.ElementAt(1));
+        response.Parent.FullPath.Should().Be("Test");
+        response.Parent.Parent.SubTree.Should().Be(folderResults.ElementAt(0));
+        response.Parent.Parent.FullPath.Should().Be("");
     }
 
     [Test]
     public async Task GivenAnInvalidRequestForAFolder_ItShouldReturnTheExpectedResult()
     {
-        try
-        {
-            // Arrange
-            var provider = CreateServiceProvider(fakeService: true, builder: c => { });
-            var sut = provider.GetRequiredService<IFoldersFacade>();
-            var folder = "Test/Folder";
-            var folderResults = provider.StubFolderCalls(folder);
-            folderResults.ElementAt(0).Folders.ElementAt(0).Name = "Wrong";
+        // Arrange
+        var provider = CreateServiceProvider(fakeService: true, builder: c => { });
+        var sut = provider.GetRequiredService<IFoldersFacade>();
+        var folder = "Test/Folder";
+        var folderResults = provider.StubFolderCalls(folder);
+        folderResults.ElementAt(0).Folders.ElementAt(0).Name = "Wrong";
 
-            // Act & Assert
-            var action = async() => await sut.GetFolderAsync(folder);
-            await action.Should().ThrowExactlyAsync<ArgumentException>()
-                .WithMessage("Unknown folder 'Test' for full path of 'Test/Folder'");
-        }
-        catch (Exception ex)
-        {
-            throw new ArgumentException("ad", ex);
-        }
+        // Act & Assert
+        var action = async() => await sut.GetFolderAsync(folder);
+        await action.Should().ThrowExactlyAsync<ArgumentException>()
+            .WithMessage("Unknown folder 'Test' for full path of 'Test/Folder'");
     }    
 }
